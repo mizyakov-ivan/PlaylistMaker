a84507015e9f1b1242ed61a56db63d4084840dde
Mizyakov Ivan <imizwork@yandex.ru>
2024-07-03 12:04:16 +0300

    Домашнее задание 14 спринт.

--- app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/PlayerActivity.kt
@@ -1,17 +1,40 @@
 package com.practicum.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.icu.text.SimpleDateFormat
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.practicum.playlistmaker.databinding.ActivityPlayerBinding
-import com.google.gson.Gson
+import java.util.Date
 
 
 class PlayerActivity : AppCompatActivity() {
 
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+
+    }
+
+    private var handler: Handler? = null
+    private var startTime = 0L
+    private var currentTime = 0L
     private lateinit var binding: ActivityPlayerBinding
+
+    private var url: String? = null
+    private var mediaPlayer = MediaPlayer()
+    private lateinit var playButton: ImageView
+    private lateinit var playTime: TextView//
+    private var playerState = STATE_DEFAULT
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,15 +42,9 @@ class PlayerActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-    }
-
-    override fun onResume() {
-        super.onResume()
-
         val track = intent.getSerializableExtra(SearchActivity.INTENT_KEY) as Track
 
-        val backButton = findViewById<ImageView>(R.id.backButton)
-        backButton.setOnClickListener {
+        binding.backButton.setOnClickListener {
             finish()
         }
 
@@ -37,7 +54,7 @@ class PlayerActivity : AppCompatActivity() {
             .placeholder(R.drawable.placeholder312)
             .transform(RoundedCorners(this.resources.getDimensionPixelSize(R.dimen.corner_2)))
             .into(binding.artwork)
-
+        url = track.previewUrl
         binding.trackName.text = track.trackName
         binding.artistName.text = track.artistName
         binding.collectionNameV.text = track.collectionName
@@ -45,6 +62,88 @@ class PlayerActivity : AppCompatActivity() {
         binding.yearV.text = track.releaseDate.substring(0, 4)
         binding.genreV.text = track.primaryGenreName
         binding.countryV.text = track.country
+        playTime = findViewById(R.id.playTime)
+        handler = Handler(Looper.getMainLooper())
+        playButton = binding.playButton
+        playTime.text = "00:00"
+
+        if (url != null) {
+            playTime
+            preparePlayer()
+            playButton.setOnClickListener {
+                playbackControl()
+            }
+        } else {
+            playButton.setOnClickListener {
+                Toast.makeText(applicationContext, "Url = null", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playButton.setImageResource(R.drawable.play_button)
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playButton.setImageResource(R.drawable.pause_button)
+        playerState = STATE_PLAYING
+        startTimer()
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playButton.setImageResource(R.drawable.play_button)
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+        when (playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    private fun startTimer() {
+        startTime = System.currentTimeMillis()
+        handler?.post(updateTimer(startTime))
+    }
+
+    private fun updateTimer(start: Long): Runnable {
+        return object : Runnable {
+            override fun run() {
+                if (playerState == STATE_PLAYING) {
+                    val current = System.currentTimeMillis()
+                    val time = current - start
+                    playTime.text = formatMilliseconds(time)
+                    handler?.postDelayed(this, 1000L)
+                }
+            }
+        }
+    }
+    fun formatMilliseconds(milliseconds: Long): String {
+        val format = SimpleDateFormat("mm:ss")
+        return format.format(Date(milliseconds))
+    }
 }
