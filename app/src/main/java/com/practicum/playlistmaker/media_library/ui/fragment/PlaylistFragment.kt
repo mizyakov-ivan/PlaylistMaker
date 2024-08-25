e2463e749a584db6959667997f612ba796881008
Mizyakov Ivan <imizwork@yandex.ru>
2024-08-25 23:07:30 +0300

    Домашнее задание 18 спринт

---
+++ app/src/main/java/com/practicum/playlistmaker/media_library/ui/fragment/PlaylistFragment.kt
@@ -0,0 +1,35 @@
+package com.practicum.playlistmaker.media_library.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.practicum.playlistmaker.databinding.FragmentPlaylistBinding
+import com.practicum.playlistmaker.media_library.ui.view_model.PlaylistViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class PlaylistFragment: Fragment() {
+    companion object{
+        fun newInstance() = PlaylistFragment()
+    }
+
+    private val playlistViewModel: PlaylistViewModel by viewModel()
+
+    private var _binding: FragmentPlaylistBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPlaylistBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/practicum/playlistmaker/media_library/ui/view_model/PlaylistViewModel.kt
@@ -0,0 +1,6 @@
+package com.practicum.playlistmaker.media_library.ui.view_model
+
+import androidx.lifecycle.ViewModel
+
+class PlaylistViewModel(): ViewModel() {
+}
