ead9954ec6d6a7e52ba7e6045a3cfc33277f75d4
Mizyakov Ivan <imizwork@yandex.ru>
2024-10-09 12:51:56 +0300

    Правки согласно рекомендаций

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,11 +12,11 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.practicum.playlistmaker"
+        applicationId = "app.playlistmaker"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
