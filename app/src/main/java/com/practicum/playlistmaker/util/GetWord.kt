dd7805b297c2a2905ac264fc01cd7cb76d10d7f6
Mizyakov Ivan <imizwork@yandex.ru>
2024-10-30 00:04:53 +0300

    Правки по замечаниям

--- app/src/main/java/com/practicum/playlistmaker/util/GetWord.kt
+++ app/src/main/java/com/practicum/playlistmaker/util/GetWord.kt
@@ -1,30 +1,14 @@
 package com.practicum.playlistmaker.util
+
 import android.view.View
 import com.practicum.playlistmaker.R
 
 object GetWord {
     fun getWordTrack(count: Int, view: View): String {
-        count % 100
-        if (count in 5..20) return view.context.getString(R.string._5_20_tracks)
-        count % 10
-        when (count) {
-            1 -> return view.context.getString(R.string._1_track)
-            in 2..4 -> return view.context.getString(R.string._2_4_track)
-            else -> return view.context.getString(R.string._5_20_tracks)
-        }
+        return view.context.resources.getQuantityString(R.plurals.track_count, count, count)
     }
 
-    fun getWordTime(count: Int, view: View): String{
-        count % 100
-        if (count in 5..20) return view.context.getString(R.string._5_20_minutes)
-        count % 10
-        when (count) {
-            1 -> return view.context.getString(R.string._1_minutes)
-
-            in 2..4 -> return view.context.getString(R.string._2_4_minutes)
-
-            else -> return view.context.getString(R.string._5_20_minutes)
-        }
+    fun getWordTime(count: Int, view: View): String {
+        return view.context.resources.getQuantityString(R.plurals.minute_count, count, count)
     }
-
 }
